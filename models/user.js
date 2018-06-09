7fc8e0e96921c0c7c8d4a5f10cbc9dc336ee3f2c
Matthew <mattsjsu14@gmail.com>
2018-06-09 13:52:22 -0700

    added column to user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    budget: DataTypes.INTEGER,
   }, {});
   User.associate = function(models) {
     // associations can be defined here
