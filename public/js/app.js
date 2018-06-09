a9dc9773fa217bb06d0a062377f95e8d827b6596
Andrew Villanueva <andrew@ANDREWs-MacBook-Pro.local>
2018-06-09 14:20:00 -0700

    added user-dashboard

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+var myConfig = {
+  "type":"pie",
+  "title":{
+    "text":"Pie Chart"
+  },
+  "series":[
+    {"values":[59]},
+    {"values":[55]},
+    {"values":[30]},
+    {"values":[28]},
+    {"values":[15]}
+  ]
+};
+ 
+zingchart.render({ 
+	id : 'myChart', 
+	data : myConfig, 
+	height: 400, 
+	width: "100%" 
+});
